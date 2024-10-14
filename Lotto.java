86b9de49521e49265b8b003297d3012932402519
Choo Hyunjoong <hjchoo19950327@gmail.com>
2024-10-14 17:14:22 +0900

    로또 코드 수정 및 완성

--- Lotto.java
+++ Lotto.java
@@ -1,47 +1,66 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Lotto {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
+        Scanner sc = new Scanner(System.in);
+        Random rand = new Random();
         System.out.println("[로또 당첨 프로그램]");
 
-        System.out.print("로또 개수를 입력해주세요(숫자 1~10):");
-        int lottoCnt = sc.nextInt();
 
-        System.out.println("[로또 결과]");
-        int[] rstLotto = newLotto();
+        ArrayList<ArrayList<Integer>> lottoNumbers = new ArrayList();
 
-        System.out.println(rstLotto);
+        //넘버 만들기
+        char alphaNum = 'A';
+        System.out.print("로또 개수를 입력해 주세요(숫자 1~10):");
+        int lottoCount = sc.nextInt();
 
-    }
-
-    //로또 결과
-    public static int[] lottoRst(){
-        int[] rstLotto = new int[6];
-        for(int i = 0; i < rstLotto.length; i++){
-            rstLotto[i] = new Random().nextInt(45) + 1;
+        //첫번째 반복문에서 각 로또 순서 출력 후 알파벳 하나씩 더하기
+        for (int i = 0; i < lottoCount; i++) {
+            ArrayList<Integer> lottoNumber = new ArrayList<>();
+            System.out.print(alphaNum + "\t");
+            alphaNum++;
+            //두번째 반복문에서 로또 번호 랜덤 출력
+            for (int j = 0; j < 5; j++) {
+                int backNumber = rand.nextInt(45) + 1;
+                while (lottoNumber.contains(backNumber)) {
+                    backNumber = rand.nextInt(45) + 1;
+                }
+                lottoNumber.add(backNumber);
+                System.out.print(backNumber + "\t");
+            }
+            lottoNumbers.add(lottoNumber);  // 그룹을 전체 리스트에 추가
+            //두번째 반복문 끝나면 줄 바꿈
+            System.out.println();
         }
-        Arrays.sort(rstLotto);
-        return rstLotto;
-    }
 
-    //로또 생성
-    public static int[] newLotto() {
-        int[] newLotto = new int[6];
+        System.out.println("[로또 발표]");
+        ArrayList<Integer> winningNumbers = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            int resultNum = rand.nextInt(45) + 1;
+            while (winningNumbers.contains(resultNum)) {
+                resultNum = rand.nextInt(45) + 1;
+            }
+            winningNumbers.add(resultNum);
+            System.out.print(resultNum + "\t");
+        }
+        System.out.println();
 
-        for (int i = 0; i < newLotto.length; i++) {
-            newLotto[i] = new Random().nextInt(45) + 1; // 0빼고 45까지 숫자 랜덤 돌리기
-            for (int j = 0; j < i; j++) {
-                if (newLotto[j] == newLotto[i]) { // 중복제거
-                    i--;
+        char alphaSecondNum = 'A';
+        System.out.println("[내 로또 결과]");
+        for (ArrayList<Integer> lotto : lottoNumbers) {
+            System.out.print(alphaSecondNum + "\t");
+            alphaSecondNum++;
+            int matchCount = 0;
+            for (int number : lotto) {
+                System.out.print(number + "\t");
+                if (winningNumbers.contains(number)) {
+                    matchCount++;
                 }
             }
+            System.out.println("=> 일치 개수: " + matchCount);
         }
-        Arrays.sort(newLotto);
-        return newLotto;
     }
 }
