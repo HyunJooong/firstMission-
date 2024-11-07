ba0e7c44648a95c36210f8823f05704e9774268f
Choo Hyunjoong <hjchoo19950327@gmail.com>
2024-11-07 23:24:11 +0900

    코테 1주차

---
+++ baekjun/programmers/cote1/Week1_1.java
@@ -0,0 +1,34 @@
+package baekjun.programmers.cote1;
+
+public class Week1_1 {
+    public static void main(String[] args){
+
+    }
+    public int solution(int n) {
+        if(n < 2 ){
+            return  0;
+        }
+
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        for (int i = 2; i * i < n; i++) {
+            if (isPrime[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrime[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
